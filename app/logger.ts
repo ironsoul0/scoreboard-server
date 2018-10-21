00ae55871b32a4a62f5b5daebb64f40b3f959d25
kekland <kk.erzhan@gmail.com>
2018-10-21 16:44:07 +0600

    Logging made better

--- app/logger.ts
+++ app/logger.ts
@@ -1,8 +1,19 @@
 import chalk from 'chalk'
 import moment from 'moment'
 
-export function log(message: string) {
-  console.log(`${chalk.bgBlue(moment.now().toString())}: ${message}`)
+export function log(...message: string[]) {
+  const unix = moment.now().toString()
+  let spacing = ''
+  for (let i = 0; i < unix.length + 2; i++) {
+    spacing += ' '
+  }
+  message.forEach((msg, i) => {
+    if (i === 0) {
+      console.log(`${chalk.bgGreen(unix)}: ${msg}`)
+    } else {
+      console.log(`${spacing}${msg}`)
+    }
+  })
 }
 
 export function error(message: string) {
--- app/server.ts
+++ app/server.ts
@@ -10,16 +10,25 @@ const mongooseurl = 'mongodb://localhost:27017/test';
 
 (async () => {
   try {
+    log(`Initializing`)
     const app: Application = express()
     app.use(bodyParser.json())
+    app.use((req, res, next) => {
+      log(`Requested ${chalk.blue(req.method)} ${chalk.blue(req.url)}`,
+          `${chalk.cyan(JSON.stringify(req.body))} by ${chalk.green(req.ip)}`,
+          `with ${chalk.blue(req.rawHeaders.toString())}`)
+      next()
+    })
     app.use('/competition', competitionRouter)
     app.use('/competition/task', taskRouter)
-    log(`Connecting to Mongoose at ${chalk.blue(mongooseurl)}`)
+
+    log(`Connecting to MongoDB at ${chalk.blue(mongooseurl)}`)
     await mongoose.connect('mongodb://localhost:27017/test')
-    log(`Connected to Mongoose at ${chalk.blue(mongooseurl)}`)
+    log(`Connected to MongoDB at ${chalk.blue(mongooseurl)}`)
 
     const port = process.env.PORT || 8080
     app.listen(port, () => {
+      log(`Initialization complete`)
       log(`Listening at port ${chalk.blue(port.toString())}`)
     })
   } catch (e) {
